c60b9c27ce3a355dcea649fbc9be59ee79d73d29
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-09-27 15:51:45 -0400

    Update First-chapter.java

--- First-chapter.java
+++ First-chapter.java
@@ -1,3 +1,3 @@
 const subtitle = "awakening";
 const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
-const img = "ch01img";
+const img = document.createElement("chapter1.png");
