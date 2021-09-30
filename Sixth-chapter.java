bc9b0d7289398b6a880b56b2529670d05133d1e9
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-09-29 20:33:21 -0400

    added title and img to each chapter

---
+++ Sixth-chapter.java
@@ -0,0 +1,3 @@
+const subtitle = "sion?";
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
+const img = "chapter6.png";
