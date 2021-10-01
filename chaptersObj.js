0a305d560d5aa8038ff3e09a8d1f0a3f387ac03d
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-09-30 21:24:17 -0400

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -1,4 +1,9 @@
 const chapter1 = "awakening";
+const subtitle = "awakening";
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
+const img = "chapter1.png";
+
+
 const chapter2 = "funny_whole_hole";
 const chapter3 = "important_stuff";
 const chapter4 = "im_stuffed!";
