d9bed59340ae5291033d1cbbd5a32e8adcc9296b
YarataDarkness <yara01@live.ca>
2021-10-02 14:12:23 -0400

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -1,4 +1,5 @@
-const chapter1 = "awakening";
+const chapter1 = "awakening"
+
 const subtitle = "awakening";
 const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
 const img = "chapter1.png";
