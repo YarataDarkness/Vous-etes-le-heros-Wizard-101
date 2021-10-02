490f4056e0d06af317fa343f68a1ad622a724bd7
YarataDarkness <yara01@live.ca>
2021-10-02 17:01:36 -0400

    nm3

--- chaptersObj.js
+++ chaptersObj.js
@@ -1,53 +1,53 @@
 const chaptersObj = {
-    chapter1 = {
+    chapter1: {
         subtitle: "awakening",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter1.png"
    },
    
-   chapter2 = {
+   chapter2: {
         subtitle: "funny_whole_hole",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter2.png"
    },
    
-   chapter3 = {
+   chapter3: {
         subtitle: "important_stuff",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter3.png"
    },
    
-   chapter4 = {
+   chapter4: {
         subtitle: "im_stuffed!",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter4.png"
    },
    
-   chapter5 = {
+   chapter5: {
         subtitle: "explo?",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter5.png"
    },
    
-   chapter6 = {
+   chapter6: {
         subtitle: "sion?",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter6.png"
    },
    
-   chapter7 = {
+   chapter7: {
         subtitle: "choose_your_hats",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter7.png"
    },
    
-   chapter8 = {
+   chapter8: {
         subtitle: "awwwweeeeh??",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter8.png"
    },
    
-   chapter9 = {
+   chapter9: {
         subtitle: "what_you_really_were",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter9.png"
