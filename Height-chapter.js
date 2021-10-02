b33a5061b00eca5889e5b977bbc7abed7f2f95ab
YarataDarkness <yara01@live.ca>
2021-10-02 16:03:11 -0400

    remove mistake chapter
    removed ex: fifth-chapter, sixth-chapter, etc

--- Height-chapter.js
+++
@@ -1,3 +0,0 @@
-const subtitle = "awwwweeeeh??";
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
-const img = "chapter8.png";
--- Seven-chapter.js
+++
@@ -1,3 +0,0 @@
-const subtitle = "choose_your_hats";
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
-const img = "chapter7.png";
