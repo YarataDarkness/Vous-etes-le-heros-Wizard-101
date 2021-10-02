30764a3b624595c521c3dfcbd539721216477e05
YarataDarkness <yara01@live.ca>
2021-10-02 16:12:52 -0400

    put all the chapters in chaptersObj

--- chaptersObj.js
+++ chaptersObj.js
@@ -1,55 +1,57 @@
-let chapter1 = {
-     subtitle: "awakening",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter1.png"
-};
-
-let chapter2 = {
-     subtitle: "funny_whole_hole",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter2.png"
-};
-
-let chapter3 = {
-     subtitle: "important_stuff",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter3.png"
-};
-
-let chapter4 = {
-     subtitle: "im_stuffed!",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter4.png"
-};
-
-let chapter5 = {
-     subtitle: "explo?",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter5.png"
-};
-
-let chapter6 = {
-     subtitle: "sion?",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter6.png"
-};
-
-let chapter7 = {
-     subtitle: "choose_your_hats",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter7.png"
-};
-
-let chapter8 = {
-     subtitle: "awwwweeeeh??",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter8.png"
-};
-
-let chapter9 = {
-     subtitle: "what_you_really_were",
-     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-     img: "chapter9.png"
+const chaptersObj = {
+    let chapter1 = {
+        subtitle: "awakening",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter1.png"
+   };
+   
+   let chapter2 = {
+        subtitle: "funny_whole_hole",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter2.png"
+   };
+   
+   let chapter3 = {
+        subtitle: "important_stuff",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter3.png"
+   };
+   
+   let chapter4 = {
+        subtitle: "im_stuffed!",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter4.png"
+   };
+   
+   let chapter5 = {
+        subtitle: "explo?",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter5.png"
+   };
+   
+   let chapter6 = {
+        subtitle: "sion?",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter6.png"
+   };
+   
+   let chapter7 = {
+        subtitle: "choose_your_hats",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter7.png"
+   };
+   
+   let chapter8 = {
+        subtitle: "awwwweeeeh??",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter8.png"
+   };
+   
+   let chapter9 = {
+        subtitle: "what_you_really_were",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        img: "chapter9.png"
+   };
 };
 
 let chapitre = chaptersObj.length;
