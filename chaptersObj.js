ab807b55c030e46fb61cc3e2587519eeb0643452
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-10-04 12:44:46 -0400

    optios: added

--- chaptersObj.js
+++ chaptersObj.js
@@ -2,55 +2,64 @@ const chaptersObj = {
     chapter1: {
         subtitle: "awakening",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter1.png"
+        img: "chapter1.png",
+        option: 
    },
    
    chapter2: {
         subtitle: "funny_whole_hole",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter2.png"
+        img: "chapter2.png",
+        option: 
    },
    
    chapter3: {
         subtitle: "important_stuff",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter3.png"
+        img: "chapter3.png",
+        option: 
    },
    
    chapter4: {
         subtitle: "im_stuffed!",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter4.png"
+        img: "chapter4.png",
+        option: 
    },
    
    chapter5: {
         subtitle: "explo?",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter5.png"
+        img: "chapter5.png",
+        option: 
    },
    
    chapter6: {
         subtitle: "sion?",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter6.png"
+        img: "chapter6.png",
+        option: 
    },
    
    chapter7: {
         subtitle: "choose_your_hats",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter7.png"
+        img: "chapter7.png",
+        option: 
    },
    
    chapter8: {
         subtitle: "awwwweeeeh??",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter8.png"
+        img: "chapter8.png",
+        option: 
    },
    
    chapter9: {
         subtitle: "what_you_really_were",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        img: "chapter9.png"
+        img: "chapter9.png",
+        option: 
    },
 };
 
