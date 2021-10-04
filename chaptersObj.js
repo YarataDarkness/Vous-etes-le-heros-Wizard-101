f5647cec78f1c34450b30647f191f7d1980e34cd
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-10-04 16:35:27 -0400

    chapter 0

--- chaptersObj.js
+++ chaptersObj.js
@@ -1,5 +1,13 @@
 const chaptersObj = {
-    chapter1: {
+
+     chapter0: {
+          subtitle: "start",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+          img: "BPXTPdU.png",
+          options: []
+     },
+       
+     chapter1: {
         subtitle: "awakening",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
         img: "chapter1.png",
