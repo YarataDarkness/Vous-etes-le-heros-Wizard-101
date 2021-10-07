9beb1fdbcdd7d6dffed90ec9931e93e25b2443e7
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-10-07 14:31:01 -0400

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -175,7 +175,7 @@ ShowTitre.innerHTML = "WIZARD 101";
 
 const ShowSummary = document.querySelector('.summary');
 
-ShowSummary.innerHTML = chapter.subtitle;
+ShowSummary.innerHTML = chapitre.subtitle;
 
 const ShowOptions = document.querySelector('div .options');
 
