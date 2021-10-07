2f22a57369a20c536173dd6dce5a558fdadb6a9d
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-10-07 12:09:58 -0400

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -181,5 +181,5 @@ const ShowOptions = document.querySelector('div .options');
 
 ShowOptions.innerHTML = "<button>Begin!</button>";
 
-    console.log(chapitre.subtitle);
+    console.log(chapitre);
 };
