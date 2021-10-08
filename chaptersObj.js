d1e168f03216d56eefddffa0deaf7432c600d695
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-10-08 15:54:40 -0400

    ShowSummary.innerHTML = chapitre.subtitle;

--- chaptersObj.js
+++ chaptersObj.js
@@ -175,7 +175,7 @@ ShowTitre.innerHTML = "WIZARD 101";
 
 const ShowSummary = document.querySelector('.summary');
 
-ShowSummary.innerHTML = "You're a wizard and today is your first schoolday! Shenaningans better not happen!";
+ShowSummary.innerHTML = chapitre.subtitle;
 
 const ShowOptions = document.querySelector('div .options');
 
