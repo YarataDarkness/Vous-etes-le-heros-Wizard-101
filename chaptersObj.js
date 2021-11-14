7d900c15acca1b974b0ab263096082e4cfe14ce7
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-11-14 17:32:37 -0500

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -267,7 +267,7 @@ if(iscurrentchapter != undefined){
 
 //code pour letape 3 4.2
 
-const iscurrentobject = localStorage.setItem("bagTaken");
+const iscurrentobject = localStorage.getItem("bagTaken");
 
 if(iscurrentobject != undefined){
 bagTaken = true;
