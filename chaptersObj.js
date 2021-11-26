c2024f5b5ed1c61c565dc3e3055ac76e2b59c1ef
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-11-26 09:40:23 -0500

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -264,8 +264,6 @@ let wantaudio = true;
 
 const ischbwantaudio = document.querySelector('.wantaudio');
 
-ischbwantaudio.setAttribute('checkbox','checked');
-
 //
 let bagTaken = false;
 localStorage.setItem("bagTaken", bagTaken);
