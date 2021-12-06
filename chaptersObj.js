db6fd958e2ebcfbd2ce1b4fd9d8c6732ff39519e
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2021-12-06 15:32:02 -0500

    Update chaptersObj.js

--- chaptersObj.js
+++ chaptersObj.js
@@ -179,6 +179,7 @@ const chaptersObj = {
   chapter102: {
     subtitle: "Of course?",
     text: "Really? We're going for this? Fine...Have fun pretending you're honest",
+    img: "102.png",
     //-need image or video here
     options: [
       {
